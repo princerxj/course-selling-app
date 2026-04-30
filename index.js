bee4580977f0b834309e9c6d0a4bbe8ae8285df6
princerxj <pr273582@gmail.com>
2026-04-30 09:50:52 +0530

    Defining the routes and adding skeleton Codes

--- index.js
+++ index.js
@@ -3,7 +3,33 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
+app.post("/user/signup", (req, res) => {
+    res.json({
+        message : "SIgnup Endpoint",
+    })
+});
 
+app.post("/user/signin", (req, res) => {
+    res.json({
+        message : "Signin endpoint"
+    })
+});
+
+app.get("/user/purchases", (req, res) => {
+    res.json({
+        message : "Purchased Courses"
+    })
+})
+
+app.post("/course/purchase", (req, res) => {
+    res.json({
+        message : "Endpoint to purchase a course"
+    })
+})
+
+app.get("/courses", (req, res) => {
+    message : 'Get all the courses'
+})
 
 app.listen(8080, () => {
     console.log(`App is lisetning at port 8080`);
