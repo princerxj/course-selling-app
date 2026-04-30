4472dd4c1cdb5511e735c3b4958e2c903971be28
princerxj <pr273582@gmail.com>
2026-04-30 14:30:33 +0530

    Added Logic to prevent buy the same purchased course again

--- routes/course.route.js
+++ routes/course.route.js
@@ -7,6 +7,17 @@ courseRouter.post("/purchase", userMiddleware, async (req, res) => {
     const userId = req.userId;
     const courseId = req.body.courseId;
 
+    const alreadyPurchased = await purchaseModel.findOne({
+        userId,
+        courseId
+    });
+
+    if (alreadyPurchased) {
+        return res.status(400).json({
+            message: "You have already purchased this course"
+        });
+    }
+
     await purchaseModel.create({
         userId,
         courseId
