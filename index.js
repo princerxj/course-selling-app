d2c079010d995039e1c8c01e214f5f0785fe88bf
princerxj <pr273582@gmail.com>
2026-04-30 10:23:04 +0530

    Added Admin Router

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const {userRouter} = require("./routes/user.route");
 const {courseRouter} = require("./routes/course.route");
+const {adminRouter} = require("./routes/admin.route")
 const app = express();
 
 app.use(express.json())
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 app.listen(8080, () => {
---
+++ routes/admin.route.js
@@ -0,0 +1,38 @@
+const {Router} = require("express");
+const adminRouter = Router();
+
+adminRouter.use(adminMiddleware);
+
+adminRouter.post("/signup", (req, res) => {
+    res.json({
+        message : "Signup Endpoint"
+    })
+})
+
+adminRouter.post("/signin", (req, res) => {
+    res.json({
+        message : "Signin Endpoint"
+    })
+})
+
+adminRouter.post("/course", (req, res) => {
+    res.json({
+        message : "course Endpoint"
+    })
+})
+
+adminRouter.put("/course", (req, res) => {
+    res.json({
+        message : "Update the Course Details"
+    })
+})
+
+adminRouter.get("/course/bulk", (req, res) => {
+    res.json({
+        message : "See all the Courses in Bulk"
+    })
+})
+
+module.exports = {
+    adminRouter : adminRouter
+}
