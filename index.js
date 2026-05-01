dee3b04bc0a05c481496ca3e166cf29a957f3efa
princerxj <pr273582@gmail.com>
2026-05-01 20:43:26 +0530

    Implemented CORS

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const {userRouter} = require("./routes/user.route");
 const {courseRouter} = require("./routes/course.route");
 const {adminRouter} = require("./routes/admin.route")
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}));
 app.use(express.json())
 
 app.use("/api/v1/user", userRouter);
